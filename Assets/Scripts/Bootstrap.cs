c33d757307f10c6a4b0dc0cc6c405c8fed76f6d0
Aleksei B <babushkin.a.g.unity@gmail.com>
2022-11-13 21:38:12 +0300

    Подключено следование камеры

--- Assets/Scripts/Bootstrap.cs
+++ Assets/Scripts/Bootstrap.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using Core;
 using Shooter.Core;
 using UnityEngine;
@@ -6,15 +7,18 @@ namespace Shooter
 {
     public class Bootstrap : MonoBehaviour
     {
-        [SerializeField] public RootObjects _rootObjects;
-        [SerializeField] public Camera _camera;
+        [SerializeField] private RootObjects _rootObjects;
+        [SerializeField] private Camera _camera;
+        [SerializeField] private CinemachineVirtualCamera _virtualCamera;
         [SerializeField] private Player _playerPrefab;
         private PlayerFactory _playerFactory;
 
         private void Start()
         {
             _playerFactory = new PlayerFactory(_playerPrefab, _rootObjects.Units);
-            _playerFactory.Create(new UiInput(_camera));
+            var player = _playerFactory.Create(new UiInput(_camera));
+            _virtualCamera.Follow = player.transform;
+            _virtualCamera.LookAt = player.transform;
             var dummy = _playerFactory.Create(new DummyInput());
             dummy.transform.position = new Vector3(10, 0, 0);
         }
