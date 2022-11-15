76839b2b08266cf9a218e9f7394b91b6a9fb553d
Aleksei B <babushkin.a.g.unity@gmail.com>
2022-11-15 21:05:04 +0300

    Убран LiteNetLib и MessagePack

--- Assets/Scripts/Network/Messages/IMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Shooter.Network.Messages
-{
-    // [MessagePack.Union(0, typeof(TurnMessage))]
-    // [MessagePack.Union(1, typeof(ChangeCommandMessage))]
-    public interface IMessage
-    {
-    }
-}
